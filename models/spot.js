a1b8bbf8d060beb10e008f497c95e46eaa4effc2
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-04-11 16:21:34 -0500

    Update spot.js

--- models/spot.js
+++ models/spot.js
@@ -1,10 +1,14 @@
 const knex = require('../knex');
 
-const SPOT_TABLE = 'spot';
+const SPOT_TABLE = 'parking_space';
 
 const getSpots = async () => {
     return await knex(SPOT_TABLE);
 }
+
+// const getOpenSpots = async () => {
+//     return await knew(SPOT_TABLE)
+// }
 
 module.exports = {
     getSpots
