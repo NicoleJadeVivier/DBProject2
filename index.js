492914e3ed07fcd28f7fc38481b5f26fa5e92233
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-11 15:56:58 -0500

    updates

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require('express');
+const knex = require('./db/knex');
 
-// Import any route handlers here.
-
-
-// Import any middleware here
-const { createModelsMiddleware, disconnectFromDatababaseMiddleware } = require('./middleware/model-middleware');
-
-// Start by defining the express app instance
 const app = express();
 const port = 3000;
 
-app.use(createModelsMiddleware);
-
-app.get('/health', (request, response, next) => {
+app.get('/health', async (request, response, next) => {
+    const query = knex('stadium').select('stadium_name');
+    const result = await query;
+    console.log(result);
     const responseBody = { status: 'up', port };
     response.json(responseBody);
-    // next() is how we tell express to continue through the middleware chain
+   
     next();
 });
 
-// The last step of a request middleware chain is to disconnect from the DB.
-app.use(disconnectFromDatababaseMiddleware);
-
-// Now that we've configured the app, make it listen for incoming requests
 app.listen(port, () => {
     console.log(`This app is listening on port ${port}`);
 });
---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    client: 'mysql',
+    debug: true,
+    connection: {
+      host : 'localhost',
+      port : 3306,
+      user : 'root',
+      password : 'password',
+      insecureAuth: true,
+      database : 'parking_schema'
+    }
+  }
+};
