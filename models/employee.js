e34b85745dbcd04fe9c407a5a79490b1cc469e3f
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-12 17:12:57 -0500

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcrypt');
 
 const EMPLOYEE_TABLE = 'employee';
 
-const createAccount = async (employee_id, username, password) => {
+const createAccount = async (username, password) => {
     console.log('Raw password:', password);
     const salt = await bcrypt.genSalt(10);
     console.log('Password salt', salt);
     const hashedPassword = await bcrypt.hash(password, salt);
     console.log('Hashed password', hashedPassword);
 
-    const query = knex(EMPLOYEE_TABLE).insert({employee_id, username, password: hashedPassword});
+    const query = knex(EMPLOYEE_TABLE).insert({username, password: hashedPassword});
     console.log('Raw query for createAccount:', query.toString());
     const result = await query;
 
