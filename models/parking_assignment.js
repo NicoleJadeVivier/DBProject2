21e4a8de7a3a320cf45a0c2131d54c4a3fc5abbe
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-12 17:12:59 -0500

    Create parking_assignment.js

---
+++ models/parking_assignment.js
@@ -0,0 +1,31 @@
+const knex = require('../db/knex');
+
+const ALLOCATION_TABLE = 'parking_assignment';
+const CAR_TABLE = 'car';
+
+const createAllocation = async (license_plate_number, vehicle_type, parking_space) => {
+    const cars =  await knex(CAR_TABLE).where('license_plate_number', license_plate_number);
+    if (cars.length == 0) {
+        const query = await knex(CAR_TABLE).insert({license_plate_number, vehicle_type});
+        cars =  await knex(CAR_TABLE).where('license_plate_number', license_plate_number);
+    }
+    const result = await knex(ALLOCATION_TABLE).insert({parking_space, license_plate: license_plate_number, vehicle_type});
+    return result;
+};
+
+const updateAllocation = async (assignment_id, license_plate_number, vehicle_type) => {
+    const cars =  await knex(CAR_TABLE).where('license_plate_number', license_plate_number);
+    if (cars.length == 0) {
+        const query = await knex(CAR_TABLE).insert({license_plate_number, vehicle_type});
+        cars =  await knex(CAR_TABLE).where('license_plate_number', license_plate_number);
+    }
+    const result = await knex(ALLOCATION_TABLE).where('assignment_id', assignment_id).update({
+        license_plate: license_plate_number, vehicle_type
+    });
+    return result;
+};
+
+module.exports = {
+    createAllocation,
+    updateAllocation
+};
