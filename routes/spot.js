88e25ab3546885775a67689f22273e3e3e289396
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-13 12:53:44 -0500

    Create spot.js

---
+++ routes/spot.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const spot = require('../models/spot');
+const router = express.Router();
+
+router.get('/:stadium?/:lot?/:available?', async (req, res, next) => {
+    try {
+        let result;
+        console.log(req.params.stadium);
+        if (req.params.stadium) {
+            if (req.params.lot) {
+                if (req.params.available) {
+                    result = await spot.getOpenSpotsFromLot(req.params.lot);
+                } else result = await spot.getSpotsFromLot(req.params.lot);
+            } else result = await spot.getSpotsFromStadium(req.params.stadium);
+        } else result = await spot.getSpots();
+    
+        res.status(201).json(result);
+    } catch (err) {
+        console.error('Failed to select all parking spaces', err);
+        res.status(500).json({message: err.toString() });
+    }
+    next();
+});
+
+
+module.exports = router;
