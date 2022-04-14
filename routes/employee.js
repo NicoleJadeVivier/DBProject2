890b39553dbd49d1e3f6f4c2f769a3f69c1b7314
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-14 11:36:22 -0500

    Update employee.js

--- routes/employee.js
+++ routes/employee.js
@@ -6,9 +6,12 @@ router.post('/', async (req, res, next) => {
     try {
         const body = req.body;
         console.log(body);
+        if (!body.hasOwnProperty('username') || !body.hasOwnProperty('password')) {
+            throw 'Please provide username and password';
+        }
         const result = await employee.createAccount(body.username, body.password);
         res.status(201).json(result);
-    } catch {
+    } catch (err) {
         console.error('Failed to create new account:', err);
         res.status(400).json({ message: err.toString() });
     }
