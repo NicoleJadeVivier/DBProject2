56c572c54e007aebbe9b32410d7b7a612d3413a3
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-04-14 20:34:45 -0500

    creating spot controller

---
+++ controllers/spot.js
@@ -0,0 +1,21 @@
+const Spot = require('../models/spot');
+
+const spotProcessor = async (req, res, next) => {
+    try {
+        let result;
+        console.log(req.query.stadium);
+        if (req.query.stadium) {
+            if (req.query.lot) {
+                if (req.query.available) {
+                    result = await spot.getOpenSpotsFromLot(req.query.lot);
+                } else result = await spot.getSpotsFromLot(req.query.lot);
+            } else result = await spot.getSpotsFromStadium(req.query.stadium);
+        } else result = await spot.getSpots();
+    
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Failed to select all parking spaces', err);
+        res.status(500).json({message: err.toString() });
+    }
+    next();
+};
