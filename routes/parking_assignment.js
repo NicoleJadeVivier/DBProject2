74e369438e39b860bcbd3d6174be03fd8c5de400
NicoleJadeVivier <nvivier@mail.smu.edu>
2022-04-14 20:43:53 -0500

    adding parking_assignment controllers

--- routes/parking_assignment.js
+++ routes/parking_assignment.js
@@ -1,50 +1,17 @@
 const express = require('express');
-const allocation = require('../models/parking_assignment');
+const controller = require('../controllers/parking_assignment');
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-    try {
-        const body = req.body;
-        console.log(body);
-        const result = await allocation.createAllocation(body.license_plate_number, body.vehicle_type, body.parking_space);
-        res.status(201).json(result);
-    } catch {
-        console.error('Failed to create new parking allocation:', err);
-        res.status(500).json({ message: err.toString() });
-    }
-
-    next();
+    controller.postController(req, res, next);
 });
 
 router.put('/:assignment_id', async (req, res, next) => {
-    try {
-        const body = req.body;
-        console.log(body);
-        console.log(req.params.assignment_id);
-        const result = await allocation.updateAllocation(req.params.assignment_id, body.license_plate_number, body.vehicle_type);
-        res.status(200).json(result);
-    } catch {
-        console.error('Failed to update parking allocation:', err);
-        res.status(500).json({ message: err.toString() });
-    }
-
-    next();
+    controller.putController(req, res, next);
 });
 
-//comented out below because not sure if it works
-
 router.delete('/:assignment_id', async(req, res, next) => {
-    try {
-        const body = req.body;
-        console.log(body);
-        const result = await allocation.deleteAllocation(req.params.assignment_id);
-        res.status(204).json(result);
-    } catch {
-        console.error('Failed to delete parking allocation:', err);
-        res.status(500).json({ message: err.toString() });
-    }
-
-    next();
-})
+    controller.deleteController(req, res, next);
+});
 
 module.exports = router;
