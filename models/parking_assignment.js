24976c9b30eb50eff6f802f6858fd8eb09f50dd1
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-16 22:16:58 -0500

    Update parking_assignment.js

--- models/parking_assignment.js
+++ models/parking_assignment.js
@@ -4,11 +4,12 @@ const ALLOCATION_TABLE = 'parking_assignment';
 const CAR_TABLE = 'car';
 
 const createAllocation = async (license_plate_number, vehicle_type, parking_space) => {
+    console.log("in controller", license_plate_number, vehicle_type, parking_space);
     const cars =  await knex(CAR_TABLE).where('license_plate_number', license_plate_number);
     if (cars.length == 0) {
         await knex(CAR_TABLE).insert({license_plate_number, vehicle_type});
     }
-    await knex(ALLOCATION_TABLE).insert({parking_space, license_plate: license_plate_number, vehicle_type});
+    const assignment_id = await knex(ALLOCATION_TABLE).insert({parking_space, license_plate: license_plate_number, vehicle_type});
     const result = await knex(ALLOCATION_TABLE).where('assignment_id', assignment_id);
     return result;
 };
@@ -28,7 +29,7 @@ const updateAllocation = async (assignment_id, license_plate_number, vehicle_typ
 const deleteAllocation = async (allocation_id) => {
     const result = await knex(ALLOCATION_TABLE).where('assignment_id', allocation_id).del();
     return result;
-}
+};
 
 
 
