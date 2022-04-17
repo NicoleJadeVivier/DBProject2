441fcf9f53eff10a8d46dc573f7a0fbe1272656b
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-16 22:17:01 -0500

    Update spot.js

--- models/spot.js
+++ models/spot.js
@@ -4,28 +4,27 @@ const SPOT_TABLE = 'parking_space';
 
 const getSpots = async () => {
     return await knex(SPOT_TABLE);
-}
+};
 
 const getOpenSpots = async () => {
     return await knex(SPOT_TABLE).where('is_available', true);
-}
+};
 
 const getSpotsFromStadium = async (stadium) => {
     return await knex(SPOT_TABLE).where('stadium_name', stadium);
-}
+};
 
 const getOpenSpotsFromStadium = async (stadium) => {
-    return await knex(SPOT_TABLE).where('stadium_name', stadium).where('is_available', true);
-    
-}
+    return await knex(SPOT_TABLE).where('stadium_name', stadium).where('is_available', true);z 
+};
 
 const getSpotsFromLot = async (parking_lot) => {
     return await knex(SPOT_TABLE).where('lot_id', parking_lot);
-}
+};
 
 const getOpenSpotsFromLot = async (parking_lot) => {
     return await knex(SPOT_TABLE).where('is_available', true).where('lot_id', parking_lot);
-}
+};
 
 
 
