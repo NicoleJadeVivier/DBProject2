d0017cc6a35f851db3b3d90e9c6306d303ddbc49
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-16 22:17:05 -0500

    Update parking_assignment.js

--- routes/parking_assignment.js
+++ routes/parking_assignment.js
@@ -1,19 +1,43 @@
 const express = require('express');
-const controller = require('../controllers/parking_assignment');
+const allocationController = require('../controllers/parking_assignment');
 const router = express.Router();
 
 router.post('/', async (req, res, next) => {
-    controller.postController(req, res);
+    try {
+        const body = req.body;
+        console.log("in route", body.license_plate_number, body.vehicle_type, body.parking_space);
+        const result = await allocationController.create(body.license_plate_number, body.vehicle_type, body.parking_space);
+        res.status(201).json(result);
+    } catch {
+        console.error('Failed to create new parking allocation:', err);
+        res.status(500).json({ message: err.toString() });
+    }
     next();
 });
 
 router.put('/:assignment_id', async (req, res, next) => {
-    controller.putController(req, res);
+    try {
+        const body = req.body;
+        console.log(req.params.assignment_id);
+       const result = await allocationController.update(req.params.assignment_id, body.license_plate_number, body.vehicle_type);
+        res.status(200).json(result);
+    } catch {
+        console.error('Failed to update parking allocation:', err);
+        res.status(500).json({ message: err.toString() });
+    }
+
     next();
 });
 
 router.delete('/:assignment_id', async(req, res, next) => {
-    controller.deleteController(req, res);
+    try {
+        const body = req.body;
+        const result = await allocationController.deleteAllocation(req.params.assignment_id);
+        res.status(204).json(result);
+    } catch {
+        console.error('Failed to delete parking allocation:', err);
+        res.status(500).json({ message: err.toString() });
+    }
     next();
 });
 
