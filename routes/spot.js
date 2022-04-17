4bb792cca834a905746f20a564b258192390668b
llmalone <78106359+llmalone@users.noreply.github.com>
2022-04-16 22:17:09 -0500

    Update spot.js

--- routes/spot.js
+++ routes/spot.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const controller = require('../controller/spot');
+const spotController = require('../controllers/spot');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    controller.spotProcessor(req, res);
+    try {
+        console.log("in route", req.query.stadium, req.query.lot, req.query.available);
+        const result = await spotController.getSpots(req.query.stadium, req.query.lot, req.query.available);
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Failed to select all parking spaces', err);
+        res.status(500).json({message: err.toString() });
+    }
     next();
 });
 
